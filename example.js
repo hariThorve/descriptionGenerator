7f6e53cbeac61aa83c016473616e987ae37e4edd
hariThorve <hariprasadthorve@gmail.com>
2025-12-18 10:16:09 +0530

    created assistant for job Description

--- example.js
+++ example.js
@@ -1,17 +1,43 @@
 import { Groq } from 'groq-sdk';
 import 'dotenv/config';
-const groq = new Groq({apiKey: process.env.DESCRIPTIO_GENERATOR_API});
+
+const groq = new Groq({apiKey: process.env.DESCRIPTION_GENERATOR_API});
+
+const input = {
+    jobTitle : "Nurse",
+    department : "Cardiology",
+    jobType : "Part Time",
+    payRange : "$2300 - $2800",
+    location : "Toronto ON",
+    experienceRequired : "2 - 3 Yrs",
+    qualification : "Neurology",
+    specialization : "Neurology",
+    urgency : "High",
+    inPersonInterview : true,
+    physicalInterview : true
+}
 
 const chatCompletion = await groq.chat.completions.create({
   "messages": [
     {
+        "role": "system",
+        "content": 
+        `You are responsible to create a short and precise Job Description
+        based on the input data, which will contain information regarding job post.
+        Keep it simple in JSON format in such format {description: "your output"}, 
+        dont create the description in markdown format.
+        It is mandatory to have json output. Explain the Job role and responsibilities,
+        so that the candidate would understand properly.
+        `
+    },
+    {
       "role": "user",
-      "content": "hey how can you help me"
+      "content": `Job details : ${JSON.stringify(input)}`
     }
   ],
   "model": "openai/gpt-oss-120b",
   "temperature": 1,
-  "max_completion_tokens": 8192,
+  "max_completion_tokens": 1000,
   "top_p": 1,
   "stream": true,
   "reasoning_effort": "medium",
