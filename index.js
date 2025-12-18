213e8e25b01f6ff8876ac01f9dec60803d3b329c
hariThorve <hariprasadthorve@gmail.com>
2025-12-18 08:56:01 +0530

    getting requirements done

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("hello world")
+// creating job description generator 
+// input = {
+//     jobTitle : String (important not null),
+//     department : string (important not null),
+//     jobType : String (important not null),
+//     payRange : String (important),
+//     location : String,
+//     experienceRequired : String,
+//     qualification : String,
+//     specialization : string,
+//     urgency : string,
+//     inPersonInterview : Boolean,
+//     physicalInterview : Boolean
+// }
+
